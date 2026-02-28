b8030d422bda7e0a3dc8d90e91c49ebac240afbc
JoyceAZIO <azio1sa001@gmail.com>
2026-02-28 20:44:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Server OK");
 });
 
+// 👇 加上這個
+app.post("/callback", (req, res) => {
+  console.log("LINE webhook hit");
+  res.status(200).send("OK");
+});
+
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
   console.log("Server started");
 });
